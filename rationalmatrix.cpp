14aff06ff5ec0e1d5ff41b2cd46e3df13c7a360c
PavelKilko <pavel.kilko@gmail.com>
2021-10-16 19:45:36 +0300

    Added files rationalmatrix.cpp and rationalmatrix.h

---
+++ rationalmatrix.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Pavel on 10/16/2021.
+//
+
+#include "rationalmatrix.h"
---
+++ rationalmatrix.h
@@ -0,0 +1,8 @@
+//
+// Created by Pavel on 10/16/2021.
+//
+
+#ifndef LAB1_RATIONALMATRIX_H
+#define LAB1_RATIONALMATRIX_H
+
+#endif //LAB1_RATIONALMATRIX_H
