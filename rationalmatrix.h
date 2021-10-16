9fddf4295c4042743715c593850ed2c1749b6685
PavelKilko <pavel.kilko@gmail.com>
2021-10-16 20:00:02 +0300

    Declared class RationalMatrix

--- rationalmatrix.h
+++ rationalmatrix.h
@@ -5,4 +5,15 @@
 #ifndef LAB1_RATIONALMATRIX_H
 #define LAB1_RATIONALMATRIX_H
 
+#include "rational.h"
+#include <vector>
+
+class RationalMatrix
+{
+public:
+    std::vector<std::vector<Rational>> field;
+    int vSize, hSize;
+
+};
+
 #endif //LAB1_RATIONALMATRIX_H
