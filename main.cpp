aa21b5ad58256027cf1bfaf3af5c1e13f3889a20
PavelKilko <pavel.kilko@gmail.com>
2021-10-17 01:32:57 +0300

    Added new test data in file main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include "rationalmatrix.h"
+#include <chrono>
 
 int main() {
-    int k = 4;
-    int d = 100;
+    int k = 8;
+    int d = 20;
 
     Rational rTest;
     std::cout << rTest << std::endl;
@@ -24,11 +25,23 @@ int main() {
     std::cout << mTest1 << std::endl;
 
     RationalMatrix mTest2;
+    auto start1 = std::chrono::high_resolution_clock::now();
     strassen_matrix_mul(mTest, mTest1, mTest2);
+    auto end1 = std::chrono::high_resolution_clock::now();
+    auto time1 = std::chrono::duration_cast<std::chrono::milliseconds>(end1 - start1);
     std::cout << mTest2 << std::endl;
 
     RationalMatrix mTest3(k, k);
+    auto start2 = std::chrono::high_resolution_clock::now();
     normal_matrix_mul(mTest, mTest1, mTest3);
+    auto end2 = std::chrono::high_resolution_clock::now();
+    auto time2 = std::chrono::duration_cast<std::chrono::milliseconds>(end2 - start2);
     std::cout << mTest3 << std::endl;
+
+    std::cout << time1.count() << std::endl;
+    std::cout << time2.count() << std::endl;
+
+    std::cout << (mTest2==mTest3) << std::endl;
+
     return 0;
 }
