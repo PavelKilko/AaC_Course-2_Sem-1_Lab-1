38c8cd5c73da25ef9f51667bc2b309eea8e4b384
PavelKilko <pavel.kilko@gmail.com>
2021-10-16 18:33:11 +0300

    Added functions gcd(int, int) and reduction(int&, int&)

--- rational.cpp
+++ rational.cpp
@@ -3,3 +3,26 @@
 //
 
 #include "rational.h"
+
+int gcd(int x, int y) // Euclidean algorithm
+{
+    if (y == 0) return x;
+    return gcd(y, x % y);
+}
+
+void reduction(int &x, int &y)
+{
+    int nod = gcd(x, y);
+    if(!nod)
+    {
+        y = 1;
+        return;
+    }
+    x /= nod;
+    y /= nod;
+    if(y < 0)
+    {
+        x *= -1;
+        y *= -1;
+    }
+}
--- rational.h
+++ rational.h
@@ -5,6 +5,9 @@
 #ifndef LAB1_RATIONAL_H
 #define LAB1_RATIONAL_H
 
+int gcd(int x, int y);
+void reduction(int &x, int &y);
+
 class Rational
 {
 public:
