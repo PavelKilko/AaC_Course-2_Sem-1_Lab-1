410d3452e2210db3fb096a1b980bf4f00f129795
PavelKilko <pavel.kilko@gmail.com>
2021-10-31 18:20:52 +0200

    Fixed minor issues

--- main.cpp
+++ main.cpp
@@ -1,21 +1,19 @@
 #include "rationalmatrix.h"
 #include <chrono>
 
-int main() {
-    int kMax = 512;
-    int d = 5;
-
+void unit_test(int kMax, long long d)
+{
     for (int k = 1; k <= kMax; k++)
     {
         std::cout << "Test#" << k << std::endl;
         std::cout << "####################################################################################################" << std::endl;
         RationalMatrix mTest1(k, k);
         mTest1.random(d);
-//        std::cout << "mTest1:\n" << mTest1 << std::endl;
+        std::cout << "mTest1:\n" << mTest1 << std::endl;
 
         RationalMatrix mTest2(k, k);
         mTest2.random(d);
-//        std::cout << "mTest2:\n" << mTest2 << std::endl;
+        std::cout << "mTest2:\n" << mTest2 << std::endl;
 
         RationalMatrix mTest3(k, k);
         auto start1 = std::chrono::high_resolution_clock::now();
@@ -29,8 +27,8 @@ int main() {
         auto end2 = std::chrono::high_resolution_clock::now();
         auto time2 = std::chrono::duration_cast<std::chrono::milliseconds>(end2 - start2);
 
-//        std::cout << "mTest3:\n" << mTest3 << std::endl;
-//        std::cout << "mTest4:\n" << mTest4 << std::endl;
+        std::cout << "mTest3:\n" << mTest3 << std::endl;
+        std::cout << "mTest4:\n" << mTest4 << std::endl;
 
         std::cout << "Execution time of multiplication by normal algorithm: " << time1.count() << std::endl;
         std::cout << "Execution time of multiplication by Strassen's algorithm: " << time2.count() << std::endl;
@@ -39,5 +37,12 @@ int main() {
 
         std::cout << "####################################################################################################" << std::endl;
     }
+}
+
+
+int main() {
+
+    unit_test(100, 12);
+
     return 0;
 }
